33f034a4da6d6d1ff90aadfc130c1d968ae9a240
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-23 09:08:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,10 +60,12 @@ def dennis_ritchies_language
 end
 
 def adding_matz
- add the following information to the top level of programmer_hash
+  programmer_hash[:yukihiro_matsumoto]= {}
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
 
-	programmer_hash = 
+  puts programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -78,9 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto]= {}
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+
     
 end
 
